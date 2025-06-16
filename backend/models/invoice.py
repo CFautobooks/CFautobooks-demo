404c47ddc670b1576149b96ffbd81d420d174925
CFautobooks <jackbcarmichael@gmail.com>
2025-06-16 17:29:16 +1000

    Update invoice.py

--- backend/models/invoice.py
+++ backend/models/invoice.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
-from core.database import Base
+from database import Base
 
 class Invoice(Base):
     __tablename__ = "invoices"
