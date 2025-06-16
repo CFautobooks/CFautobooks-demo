caf2f0996338f409cf645e121be8ed1f5f5d059a
CFautobooks <jackbcarmichael@gmail.com>
2025-06-16 17:05:29 +1000

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    future=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    future=True
+)
 
 def get_db():
     db = SessionLocal()
