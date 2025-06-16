3ec0fd27bd3bf6d44aef2bc8d949ff9554ed3661
CFautobooks <jackbcarmichael@gmail.com>
2025-06-16 19:28:25 +1000

    Update config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
     STRIPE_API_KEY: str
     STRIPE_WEBHOOK_SECRET: str
     OCR_SERVICE_URL: str
-    SENDGRID_API_KEY: str
-    FROM_EMAIL: str
+    SENDGRID_API_KEY: optional str
+    FROM_EMAIL: cfautobooks@gmail.com str
 
     class Config:
         env_file = ".env"
