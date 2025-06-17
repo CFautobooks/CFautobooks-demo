4b09ed9d8f5ef7966027daec15cb9f262bdf4dcf
CFautobooks <jackbcarmichael@gmail.com>
2025-06-17 11:41:50 +1000

    Update database.py

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,13 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from core.config import settings
+# backend/core/database.py
 
-# ← Define and export Base here:
-Base = declarative_base()
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
