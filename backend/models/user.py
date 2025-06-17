fef294286376697b761b91e22b9d9936a79b0424
CFautobooks <jackbcarmichael@gmail.com>
2025-06-17 13:30:15 +1000

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, Integer, String
-from core.database import Base
+from backend.core.database import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    plan = Column(String, nullable=False)
